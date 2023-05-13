3b722433c7c9522fc99b5077c81613a32d0317b3
kedicode <kedicode@gmail.com>
2023-05-13 14:55:26 -0400

    work on layout

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 window = tk.Tk()
-window.geometry('600x400')
+window.geometry('600x500')
 window.title('OurPasswords')
 
 # header widget
@@ -17,19 +17,26 @@ middle_left = ttk.Frame(middle_frame)
 table = ttk.Treeview(middle_left, columns=('site', 'username'), show='headings')
 table.heading('site', text='Site')
 table.heading('username', text='UserName')
+
+# card section
 middle_right = ttk.Frame(middle_frame)
-middle_right_label = ttk.Label(middle_right, text='Text Label', background='yellow')
-table.pack(side='left', expand=1, fill='both')
-middle_right_label.pack(side='left', expand=1, fill='both')
-middle_left.pack(side='left', expand=1, fill='both')
-middle_right.pack(side='left', expand=1, fill='both')
-middle_frame.pack(expand=1, fill='both')
+card_search = tk.StringVar()
+card_site = tk.StringVar()
+card_user = tk.StringVar()
+card_search_label = ttk.Label(middle_right, text='Search For existing site and user')
+card_search_entry = ttk.Entry(middle_right, textvariable=card_search)
+card_search_sep = ttk.Separator(middle_right)
+card_site_label = ttk.Label(middle_right, text='Site')
+card_site_entry = ttk.Entry(middle_right, textvariable=card_site)
+card_user_label = ttk.Label(middle_right, text='UserName')
+card_user_entry = ttk.Entry(middle_right, textvariable=card_user)
 
 # bottom section
 bottom_frame = ttk.Frame(window)
 
 # edit/save entries
 bottom_left = ttk.Frame(bottom_frame)
+bottom_edit_label = ttk.Label(bottom_left, text='Add/Edit Entry', font=('Times', 15,))
 site_name = tk.StringVar()
 site_url = tk.StringVar()
 user_name = tk.StringVar()
@@ -53,9 +60,26 @@ bottom_radio_pick = ttk.Radiobutton(bottom_right, text='Pick for me', variable=p
 bottom_radio_choose = ttk.Radiobutton(bottom_right, text='Use my own', variable=password_pick, value=0)
 button_frame = ttk.Frame(bottom_right)
 save_button = ttk.Button(button_frame, text='Save')
-delete_button = ttk.Button(button_frame, text='Delete')
+delete_button = ttk.Button(button_frame, text='Delete/Cancel')
+
+
+# <editor-fold desc="Master Layout">
+# table layout
+table.pack(side='left', expand=1, fill='y')
+middle_left.pack(side='left', expand=1, fill='y', padx=10)
+
+# result layout
+card_search_label.pack(expand=1, fill='x')
+card_search_entry.pack(expand=1, fill='x')
+card_search_sep.pack(expand=1, fill='x')
+card_site_label.pack(expand=1, fill='x')
+card_site_entry.pack(expand=1, fill='x')
+card_user_label.pack(expand=1, fill='x')
+card_user_entry.pack(expand=1, fill='x')
+middle_right.pack(side='left', expand=1, fill='both', padx=10)
 
 # save/edit layout
+bottom_edit_label.pack(fill='x', expand=1)
 site_name_label.pack(fill='x', expand=1)
 site_name_entry.pack(fill='x', expand=1)
 site_url_label.pack(fill='x', expand=1)
@@ -64,19 +88,22 @@ user_name_label.pack(fill='x', expand=1)
 user_name_entry.pack(fill='x', expand=1)
 password_label.pack(fill='x', expand=1)
 password_entry.pack(fill='x', expand=1)
-bottom_left.pack(side='left', expand=1, fill='both')
-
+bottom_left.pack(side='left', expand=1, fill='both', padx=10, pady=10)
 # Password Generate layout
 bottom_generate_label.pack(expand=1, fill='x')
 bottom_rule1.pack(expand=1, fill='x')
 bottom_radio_pick.pack(expand=1, fill='x')
 bottom_radio_choose.pack(expand=1, fill='x')
 bottom_rule2.pack(expand=1, fill='x')
-bottom_right.pack(side='left', expand=1, fill='both')
 save_button.pack(side='left', expand=1, fill='both')
 delete_button.pack(side='left', expand=1, fill='both')
 button_frame.pack(expand=1, fill='both')
 
+bottom_right.pack(side='left', expand=1, fill='both', pady=10, padx=10)
+
+# frame layouts
+middle_frame.pack(expand=1, fill='both')
 bottom_frame.pack(expand=1, fill='both')
+# </editor-fold>
 
 window.mainloop()
